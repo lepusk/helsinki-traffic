1fddc2042e0144e774b1ae7879438f5d64f2d4e6
Markus P <markus.palviainen@icloud.com>
2015-10-29 15:50:46 +0200

    json datasets

---
+++ app/public/js/coordinates.js
@@ -0,0 +1,328 @@
+window.coordinatesData = [
+  {
+    measurementPoint: 'A01',
+    name: 'LAUTTASAAREN SILTA',
+    lon: 24.89961514,
+    lat: 60.16238533
+  },
+  {
+    measurementPoint: 'A02',
+    name: 'LAPINLAHDEN SILTA',
+    lon: 24.90235664,
+    lat: 60.16709052
+  },
+  // {
+  //   measurementPoint: 'A03',
+  //   name: 'MERIKANNONTIE',
+  //   lon: 24.91338728,
+  //   lat: 60.17802129
+  // },
+  {
+    measurementPoint: 'A04',
+    name: 'MECHELININKATU',
+    lon: 24.9178601,
+    lat: 60.17628291
+  },
+  {
+    measurementPoint: 'A05',
+    name: 'RUNEBERGINKATU',
+    lon: 24.92227315,
+    lat: 60.17676126
+  },
+  // {
+  //   measurementPoint: 'A06',
+  //   name: 'TÖÖLÖNKATU',
+  //   lon: 24.92888534,
+  //   lat: 60.17684576
+  // },
+  {
+    measurementPoint: 'A07',
+    name: 'MANNERHEIMINTIE',
+    lon: 24.92951416,
+    lat: 60.17768081
+  },
+  {
+    measurementPoint: 'A08',
+    name: 'PITKÄSILTA',
+    lon: 24.95020087,
+    lat: 60.17621822
+  },
+  {
+    measurementPoint: 'A09',
+    name: 'HAKANIEMEN SILTA',
+    lon: 24.9574068,
+    lat: 60.17691184
+  },
+  {
+    measurementPoint: 'B01',
+    name: 'LÄNSIVÄYLÄ',
+    lon: 24.84501359,
+    lat: 60.16551902
+  },
+  {
+    measurementPoint: 'B02',
+    name: 'MEILAHDEN SILTA',
+    lon: 24.88375116,
+    lat: 60.19230545
+  },
+  {
+    measurementPoint: 'B03',
+    name: 'MUNKKINIEMEN SILTA',
+    lon: 24.88770495,
+    lat: 60.19579132
+  },
+  {
+    measurementPoint: 'B04',
+    name: 'MANNERHEIMINTIE',
+    lon: 24.89908485,
+    lat: 60.2061132
+  },
+  {
+    measurementPoint: 'B05',
+    name: 'ILMALANKATU',
+    lon: 24.91511466,
+    lat: 60.20686041
+  },
+  {
+    measurementPoint: 'B06',
+    name: 'VETURITIE',
+    lon: 24.92727383,
+    lat: 60.20489305
+  },
+  {
+    measurementPoint: 'B07',
+    name: 'RATAPIHANTIE',
+    lon: 24.93299007,
+    lat: 60.20496786
+  },
+  {
+    measurementPoint: 'B08',
+    name: 'MÄKELÄNKATU',
+    lon: 24.94578408,
+    lat: 60.19966925
+  },
+  {
+    measurementPoint: 'B09',
+    name: 'HÄMEENTIEN SILTA',
+    lon: 24.96307274,
+    lat: 60.20146129
+  },
+  {
+    measurementPoint: 'B10',
+    name: 'HERMANNIN RANTATIE',
+    lon: 24.96920615,
+    lat: 60.19835737
+  },
+  // no data
+  // {
+  //   measurementPoint: 'B11',
+  //   name: 'KYLÄSAARENKATU',
+  //   lon: 24.97052184,
+  //   lat: 60.19885132
+  // },
+  {
+    measurementPoint: 'B12',
+    name: 'KULOSAAREN SILTA',
+    lon: 24.98655524,
+    lat: 60.18611781
+  },
+  // duplicate
+  // {
+  //   measurementPoint: 'C01',
+  //   name: 'LÄNSIVÄYLÄ',
+  //   lon: 24.84411249,
+  //   lat: 60.16563464
+  // },
+  {
+    measurementPoint: 'C02',
+    name: 'OTANIEMEN SILTA',
+    lon: 24.84367992,
+    lat: 60.18086547
+  },
+  {
+    measurementPoint: 'C03',
+    name: 'TURUNVÄYLÄ',
+    lon: 24.84670119,
+    lat: 60.20535395
+  },
+  // {
+  //   measurementPoint: 'C04',
+  //   name: 'TURUNTIE',
+  //   lon: 24.84832666,
+  //   lat: 60.22434775
+  // },
+  // {
+  //   measurementPoint: 'C05',
+  //   name: 'KEHÄ 1',
+  //   lon: 24.83796087,
+  //   lat: 60.22979253
+  // },
+  // {
+  //   measurementPoint: 'C06',
+  //   name: 'VIHDINTIE',
+  //   lon: 24.83232993,
+  //   lat: 60.25107509
+  // },
+  // {
+  //   measurementPoint: 'C07',
+  //   name: 'VANHA NURMIJÄRVENTIE',
+  //   lon: 24.86696249,
+  //   lat: 60.25182336
+  // },
+  // {
+  //   measurementPoint: 'C08',
+  //   name: 'HÄMEENLINNANVÄYLÄ',
+  //   lon: 24.87704408,
+  //   lat: 60.26015321
+  // },
+  // {
+  //   measurementPoint: 'C09',
+  //   name: 'TUUSULANVÄYLÄ',
+  //   lon: 24.9672476,
+  //   lat: 60.26406548
+  // },
+  // {
+  //   measurementPoint: 'C10',
+  //   name: 'VALIMOT.+KIRKKOT.',
+  //   lon: 24.97201545,
+  //   lat: 60.2656911
+  // },
+  // {
+  //   measurementPoint: 'C11',
+  //   name: 'SUUTARILANTIE',
+  //   lon: 25.01180299,
+  //   lat: 60.28169648
+  // },
+  // {
+  //   measurementPoint: 'C12',
+  //   name: 'TIKKURITIE',
+  //   lon: 25.02863045,
+  //   lat: 60.28123618
+  // },
+  // {
+  //   measurementPoint: 'C13',
+  //   name: 'V.PORVOONTIE',
+  //   lon: 25.07681904,
+  //   lat: 60.27405477
+  // },
+  // {
+  //   measurementPoint: 'C14',
+  //   name: 'LAHDENVÄYLÄ',
+  //   lon: 25.08550533,
+  //   lat: 60.27184154
+  // },
+  // {
+  //   measurementPoint: 'C15',
+  //   name: 'PORVOONVÄYLÄ',
+  //   lon: 25.08426629,
+  //   lat: 60.25377502
+  // },
+  // {
+  //   measurementPoint: 'C16',
+  //   name: 'LÄNSIMÄENTIE',
+  //   lon: 25.10715798,
+  //   lat: 60.24028653
+  // },
+  // {
+  //   measurementPoint: 'C17',
+  //   name: 'ITÄVÄYLÄ',
+  //   lon: 25.13402965,
+  //   lat: 60.23388064
+  // },
+  // {
+  //   measurementPoint: 'D01',
+  //   name: 'KEHÄ 1',
+  //   lon: 24.91192791,
+  //   lat: 60.2385414
+  // },
+  // {
+  //   measurementPoint: 'D02',
+  //   name: 'PIRKKOLANTIE',
+  //   lon: 24.91415397,
+  //   lat: 60.22944184
+  // },
+  // {
+  //   measurementPoint: 'D03',
+  //   name: 'METSÄLÄNTIE',
+  //   lon: 24.92070896,
+  //   lat: 60.22022822
+  // },
+  // {
+  //   measurementPoint: 'D04',
+  //   name: 'HAKAMÄENTIE',
+  //   lon: 24.9079332,
+  //   lat: 60.20828271
+  // },
+  // {
+  //   measurementPoint: 'D05',
+  //   name: 'NORDENSKIÖLDINKATU',
+  //   lon: 24.92595125,
+  //   lat: 60.19163565
+  // },
+  // {
+  //   measurementPoint: 'D06',
+  //   name: 'HELSINGINKATU',
+  //   lon: 24.93324728,
+  //   lat: 60.18487205
+  // },
+  // {
+  //   measurementPoint: 'D07',
+  //   name: 'KAIVOKATU',
+  //   lon: 24.94100461,
+  //   lat: 60.17038939
+  // },
+  // {
+  //   measurementPoint: 'D10',
+  //   name: 'ETELÄESPLANADI',
+  //   lon: 24.94502729,
+  //   lat: 60.16704328
+  // },
+  // {
+  //   measurementPoint: 'D11',
+  //   name: 'TEHTAANKATU',
+  //   lon: 24.95074955,
+  //   lat: 60.15872529
+  // },
+  // {
+  //   measurementPoint: 'D12',
+  //   name: 'PUISTOKATU',
+  //   lon: 24.95213874,
+  //   lat: 60.1569307
+  // },
+  // {
+  //   measurementPoint: 'D13',
+  //   name: 'EHRENSTRÖMINTIE',
+  //   lon: 24.95278984,
+  //   lat: 60.15492941
+  // },
+  // {
+  //   measurementPoint: 'F01',
+  //   name: 'KEHÄ III',
+  //   lon: 25.14617324,
+  //   lat: 60.24532038
+  // },
+  // {
+  //   measurementPoint: 'F02',
+  //   name: 'SOTUNGINTIE',
+  //   lon: 25.15533963,
+  //   lat: 60.25558674
+  // },
+  // {
+  //   measurementPoint: 'F03',
+  //   name: 'PORVOONVÄYLÄ 1',
+  //   lon: 25.14570243,
+  //   lat: 60.26127007
+  // },
+  // {
+  //   measurementPoint: 'F05',
+  //   name: 'PORVOONVÄYLÄ 2',
+  //   lon: 25.25155434,
+  //   lat: 60.27512227
+  // },
+  // {
+  //   measurementPoint: 'F06',
+  //   name: 'UUSI PORVOONTTIE',
+  //   lon: 25.23040447,
+  //   lat: 60.26138349
+  // }
+];
